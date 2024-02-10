7da5e9eb483b04bed778f3d201a8e4f053847082
Mason Hall <hallmasonschool@gmail.com>
2024-02-10 12:19:50 -0500

    style for profile card
    enlarged profile card

--- src/profilePage.js
+++ src/profilePage.js
@@ -8,12 +8,27 @@ function profilePage() {
       <div className="name">Josh Giddy</div>
       <div className="player_content">
         <div className="profile">
-        <img src={'https://cdn.nba.com/headshots/nba/latest/1040x760/1630581.png'} alt="Profile" />
+        <img src={'https://cdn.nba.com/headshots/nba/latest/1040x760/1630581.png'} />
         </div>
+       < div classname="otherplayercontent">
+       <div className="player_content_text">
+          Overall
+        </div>
+        <div className="player_content_circle">
+          86
+        </div>
+       </div>
+       < div classname="otherplayercontent">
+       <div className="player_content_text">
+          Position
+        </div>
+        <div className="player_content_circle1">
+          PF
+        </div>
+       </div>
       </div>
     </div>
   </div>
   );
 }
-
 export default profilePage;
