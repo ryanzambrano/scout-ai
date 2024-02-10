0ea867551f9ee841e335e06318504b6afb5c61c4
Mason Hall <hallmasonschool@gmail.com>
2024-02-10 17:09:40 -0500

    Pushhhhhh

--- src/profilePage.js
+++ src/profilePage.js
@@ -1,34 +1,69 @@
 import React, { useState } from "react";
 import "./profilePage.css";
 
-function profilePage() {
+function ProfilePage() {
+  const [selectedTab, setSelectedTab] = useState("Stats");
+  const tableData = [
+    "Games Played", "Minutes Per Game", "Points Per Game", "Field Goals Made",
+    "Field Goals Assisted", "Field Goal Percent", "3 Points Made", "3 Points Assisted",
+    "3 Point Percentage", "Free Throws Made", "Free Throws Attempted", "Free Throw Percentage",
+    "Offensive Rebounds", "Defensive Rebounds", "Rebounds Per Game", "Assist Per Game",
+    "Steals Per Game", "Blocks Per Game", "Turnovers", "Person Fouls",
+  ];
+
   return (
     <div className="container">
-    <div className="card">
       <div className="name">Josh Giddy</div>
-      <div className="player_content">
-        <div className="profile">
-        <img src={'https://cdn.nba.com/headshots/nba/latest/1040x760/1630581.png'} />
+      <div className="card">
+        <div className="cardheader">
+          <div
+            className={`tab ${selectedTab === "Stats" ? "selected" : ""}`}
+            onMouseEnter={() => setSelectedTab("Stats")}
+          >
+            Stats
+          </div>
+          <div
+            className={`tab ${selectedTab === "Prediction" ? "selected" : ""}`}
+            onMouseEnter={() => setSelectedTab("Prediction")}
+          >
+            Prediction
+          </div>
+          <div
+            className={`tab ${selectedTab === "Overview" ? "selected" : ""}`}
+            onMouseEnter={() => setSelectedTab("Overview")}
+          >
+            Overview
+          </div>
         </div>
-       < div classname="otherplayercontent">
-       <div className="player_content_text">
-          Overall
+
+        <div className="tabContent">
+          {selectedTab === "Stats" && (
+  <div className="statsContent">
+    <table>
+      <tbody>
+        {Array.from({ length: tableData.length / 4 }, (_, rowIndex) => (
+          <tr key={rowIndex}>
+            {Array.from({ length: 4 }, (_, colIndex) => {
+              const dataIndex = rowIndex * 4 + colIndex;
+              return (
+                <td key={colIndex}>
+                  <div className="boxNumber">{dataIndex + 1}</div>
+                  <div className="boxText">{tableData[dataIndex]}</div>
+                </td>
+              );
+            })}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+)}
+          {selectedTab === "Prediction" && <p>Prediction content goes here</p>}
+          {selectedTab === "Overview" && <p>Overview content goes here</p>}
         </div>
-        <div className="player_content_circle">
-          86
-        </div>
-       </div>
-       < div classname="otherplayercontent">
-       <div className="player_content_text">
-          Position
-        </div>
-        <div className="player_content_circle1">
-          PF
-        </div>
-       </div>
       </div>
     </div>
-  </div>
   );
 }
-export default profilePage;
+
+export default ProfilePage;
